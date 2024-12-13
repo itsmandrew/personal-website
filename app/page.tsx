17940a8eb1bed3cab0ba24401187e7d229bd7196
itsmandrew <theachang1688@gmail.com>
2024-12-13 15:12:42 -0800

    Added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/features/Hero/Hero";
 import About from "@/components/features/About/About";
-
+import Experience from "@/components/features/Experience/Experience";
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Experience />
     </main>
   );
 }
