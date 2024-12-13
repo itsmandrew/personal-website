811d8bd6365610dfab16cf00c82df6ee1459f4d2
itsmandrew <theachang1688@gmail.com>
2024-12-12 21:19:31 -0800

    Created about + moved images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/features/Hero/Hero";
+import About from "@/components/features/About/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
--- components/features/About/About.tsx
+++ components/features/About/About.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./About.module.css";
 
 export default function About() {
   const el = useRef(null);
@@ -18,96 +19,110 @@ export default function About() {
       loopCount: Infinity,
     });
 
+    // Animation observer
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.show);
+        }
+      });
+    });
+
+    // Observe all sliding elements
+    const slidingElements = document.querySelectorAll(
+      `.${styles.hidden}, .${styles.hidden2}`
+    );
+    slidingElements.forEach((el) => observer.observe(el));
+
     return () => {
       typed.destroy();
+      observer.disconnect();
     };
   }, []);
 
   return (
-    <div
-      id="about"
-      className="container-fluid d-flex flex-column"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="container-fluid d-flex flex-column text-center">
-        <p className="display-4 pb-3" style={{ marginBottom: "50px" }}>
-          About my <span ref={el} className="red"></span>
-        </p>
-      </div>
+    <>
+      {/* Spacing */}
       <div
-        id="content"
-        className="d-flex flex-wrap flex-row align-items-center justify-content-center"
+        className="container-fluid d-flex flex-column"
+        style={{ minHeight: "50vh" }}
+      ></div>
+
+      <div
+        id="about"
+        className="container-fluid d-flex flex-column"
+        style={{ minHeight: "100vh" }}
       >
-        <div className="d-flex flex-column align-items-center justify-content-center half red hidden">
-          <Image
-            src="/static/profile_pic.jpg"
-            width={400}
-            height={400}
-            className="about-image"
-            alt="me being hella sexy"
-          />
-          <div
-            className="d-flex flex-row align-items-center"
-            style={{ paddingTop: "3vh", paddingBottom: "3vh" }}
-          >
-            <Link
-              href="/static/resume.pdf"
-              style={{ fontSize: "larger", color: "#ff001e" }}
-            >
-              this is my resume btw
-            </Link>
-          </div>
+        <div className="container-fluid d-flex flex-column text-center">
+          <p className="display-4 pb-3" style={{ marginBottom: "50px" }}>
+            About my <span ref={el} className="red"></span>
+          </p>
         </div>
-        <div className="half hidden2">
+        <div
+          id="content"
+          className={`d-flex flex-wrap flex-row align-items-center justify-content-center ${styles.content}`}
+        >
           <div
-            className="container mx-0 my-3 py-3"
-            style={{
-              backgroundColor: "rgb(41, 37, 46)",
-              borderRadius: "15px 50px 30px 5px",
-            }}
+            className={`d-flex flex-column align-items-center justify-content-center ${styles.half} red ${styles.hidden}`}
           >
-            <h3 className="red">Who am I?</h3>
-            <ul>
-              <li>
-                My name is <strong>Andrew Chang</strong>
-              </li>
-              <li>
-                I&apos;m currently a 4th year Computer Science student at UCI
-              </li>
-              <li>I&apos;m from Diamond Bar, CA and was born in SoCal</li>
-            </ul>
+            <Image
+              src="/profile_pic.jpg"
+              width={400}
+              height={400}
+              className={styles.aboutImage}
+              alt="me being hella sexy"
+            />
+            <div
+              className="d-flex flex-row align-items-center"
+              style={{ paddingTop: "3vh", paddingBottom: "3vh" }}
+            >
+              <Link
+                href="/static/resume.pdf"
+                style={{
+                  fontSize: "larger",
+                  color: "#ff001e",
+                }}
+                className={styles.resumeText}
+              >
+                this is my resume btw
+              </Link>
+            </div>
           </div>
-          <div
-            className="container mx-0 my-3 py-3"
-            style={{
-              backgroundColor: "rgb(41, 37, 46)",
-              borderRadius: "15px 50px 30px 5px",
-            }}
-          >
-            <h3 className="red">What are my skills?</h3>
-            <ul>
-              <li>Python, TypeScript, FastAPI, Next.js, React</li>
-              <li>Full stack development, AI engineering, data engineering</li>
-              <li>I love BUILDING</li>
-            </ul>
+          <div className={`${styles.half} ${styles.hidden2}`}>
+            <div className={styles.infoContainer}>
+              <h3>Who am I?</h3>
+              <ul>
+                <li>
+                  My name is <strong>Andrew Chang</strong>
+                </li>
+                <li>
+                  I&apos;m currently a 4th year Computer Science student at UCI
+                </li>
+                <li>I&apos;m from Diamond Bar, CA and was born in SoCal</li>
+              </ul>
+            </div>
+            <div className={styles.infoContainer}>
+              <h3>What are my skills?</h3>
+              <ul>
+                <li>Python, TypeScript, FastAPI, Next.js, React</li>
+                <li>
+                  Full stack development, AI engineering, data engineering
+                </li>
+                <li>I love BUILDING</li>
+              </ul>
+            </div>
+            <div className={styles.infoContainer}>
+              <h3>What are my hobbies?</h3>
+              <ul>
+                <li>Watching basketball</li>
+                <li>Gyming</li>
+                <li>Dj-ing</li>
+              </ul>
+            </div>
+            <br />
           </div>
-          <div
-            className="container mx-0 my-3 py-3"
-            style={{
-              backgroundColor: "rgb(41, 37, 46)",
-              borderRadius: "15px 50px 30px 5px",
-            }}
-          >
-            <h3 className="red">What are my hobbies?</h3>
-            <ul>
-              <li>Basketball</li>
-              <li>Gyming + powerlifting</li>
-              <li>Djing</li>
-            </ul>
-          </div>
-          <br />
         </div>
       </div>
-    </div>
+    </>
   );
 }
